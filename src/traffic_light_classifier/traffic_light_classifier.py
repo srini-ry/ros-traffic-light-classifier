1b0c8eccd01af75259eef1d9f3fb66aa09c8c8a7
Eric Gonzalez <eric@plasticgnome.com>
2017-01-15 12:02:29 -0800

    Light Classifier Node

---
+++ src/traffic_light_classifier/traffic_light_classifier.py
@@ -0,0 +1,96 @@
+import numpy
+import sys
+import PIL
+import message_filters
+import rospy
+
+from PIL import Image
+from cv_bridge import CvBridge
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from road_wizard.msg import Signals
+from runtime_manager.msg import traffic_light
+from sensor_msgs.msg import Image
+
+RED = 0
+GREEN = 1
+UNKNOWN = 2
+RADIUS_MULTIPLIER = 6
+
+# Optional tlr_result
+
+def calculate_bounds(roi_signal):
+  xmin = sys.maxint
+  xmax = -sys.maxint -1
+  ymin = sys.maxint
+  ymax = -sys.maxint -1
+  radius_max = 0
+
+
+
+  for signal in roi_signal.Signals:
+    x = signal.u
+    y = signal.v
+    radius_max = max(radius_max, signal.radius)
+    xmin = min(xmin, x)
+    xmax = max(xmax, x)
+    ymin = min(ymin, y)
+    ymax = max(ymax, y)
+
+  return int(xmin - RADIUS_MULTIPLIER * radius_max), int(xmax + RADIUS_MULTIPLIER * radius_max), int(ymin - RADIUS_MULTIPLIER * radius_max), int(ymax + RADIUS_MULTIPLIER * radius_max)
+
+
+def crop_image(image, xmin, xmax, ymin, ymax):
+  return image.crop((xmin, ymin, xmax, ymax))
+
+model = None
+
+def predict_light(cropped_roi):
+  # Load CNN Model
+  global model
+  if not model:
+    model = load_model('light_classifier_model.h5')
+  image_array = img_to_array(cropped_roi.resize((64, 64), PIL.Image.ANTIALIAS))
+  prediction = model.predict(image_array[None, :])
+  if prediction[0][0] == 1:
+    return GREEN
+  elif prediction[0][1] == 1:
+    return RED
+  else:
+    return UNKNOWN
+
+def detect_signal(roi_signal, camera_image):
+
+  # No signals available
+  if len(roi_signal.Signals) == 0:
+    return
+
+  # Convert the image to PIL
+  cv_bridge = CvBridge()
+  cv_image = cv_bridge.imgmsg_to_cv2(camera_image, "rgb8")
+  image = PIL.Image.fromarray(cv_image)
+
+  # Find the bounds of the signal
+  xmin, xmax, ymin, ymax = calculate_bounds(roi_signal)
+
+  # Crop the image for the ROI
+  cropped_roi = crop_image(image, xmin, xmax, ymin, ymax)
+
+  roi_image.publish(cv_bridge.cv2_to_imgmsg(numpy.array(cropped_roi), "rgb8"))
+  # Run the cropped image through the NN
+  prediction = predict_light(cropped_roi)
+
+  # Publish the prediction
+  light_detected_publisher.publish(traffic_light(traffic_light=prediction))
+
+
+rospy.init_node('traffic_light_classifier')
+
+roi_signal = message_filters.Subscriber('roi_signal', Signals)
+camera_image = message_filters.Subscriber('image_raw', Image)
+light_detected_publisher = rospy.Publisher('light_color', traffic_light, queue_size=1)
+roi_image = rospy.Publisher('roi_image', Image, queue_size=1)
+
+time_sync = message_filters.ApproximateTimeSynchronizer([roi_signal, camera_image], 5, .1)
+time_sync.registerCallback(detect_signal)
+rospy.spin()

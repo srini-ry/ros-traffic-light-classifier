200459f7aea5cf923c8029cf3b4da8dc75c35d71
Eric Gonzalez <eric@plasticgnome.com>
2017-01-15 12:57:36 -0800

    Update Predict with Examples

--- model/predict.py
+++ model/predict.py
@@ -9,13 +9,11 @@ def reshape_image(image):
   x = img_to_array(image.resize((64, 64), Image.ANTIALIAS))
   return x[None, :]
 
-model = load_model('light_model.h5')
+model = load_model('light_classifier_model.h5')
 
 # Load Example Image
-green = load_img('green.jpg')
-red = load_img('red.jpg')
+green = load_img('examples/green.jpg')
+red = load_img('examples/red.jpg')
 
 print 'Green' + model.predict(reshape_image(green)).__str__()
 print 'Red' + model.predict(reshape_image(red)).__str__()
-# print 'ROS Unknown' + model.predict(reshape_image(load_img('ros-frames/493.jpg'))).__str__()
-# print 'ROS Red' + model.predict(reshape_image(load_img('ros-frames/535.jpg'))).__str__()
